4c416aedc2e68965129cca50f877279309e884f3
Rahul Pillai <rahulpillai81@gmail.com>
2023-10-07 15:18:51 +0530

    Fixed build Error

---
+++ app/lib/authOptions.ts
@@ -0,0 +1,80 @@
+import  { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import CredentialsProvider from "next-auth/providers/credentials";
+import dbConnect from "./dbConnect";
+import User from '../models/users.model'
+import bcrypt from "bcrypt";
+import { isEqual } from "lodash";
+
+let cacheUser = {};
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID ?? "",
+      clientSecret: process.env.FACEBOOK_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        try {
+          if (!email || !password) {
+            console.log("null");
+            return null;
+          }
+          await dbConnect();
+          const user = await User.findOne({ email: email });
+          if (!user) throw Error("email/password mismatch!");
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (!passwordsMatch) {
+            throw Error("email/password mismatch!");
+          }
+          return user;
+        } catch (error) {
+          console.log("Error: ", error);
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile, email }) {
+      try {
+        await dbConnect();
+        const email = user.email || profile?.email;
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+          // If user doesn't exist, create a new one
+          const newUser = new User({
+            email,
+            name: user.name,
+            image: user.image,
+          });
+          await newUser.save();
+        } else {
+          // If user exists, update their image (and other fields if needed)
+          console.log(existingUser);
+          existingUser.name = user.name;
+          existingUser.image = user.image;
+          await existingUser.save();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      return true; // Return true to allow the sign-in to proceed
+    }
+  },
+  session: {
+    strategy: "jwt",
+  },
+};
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -16,11 +16,11 @@ function SignUp() {
   const [loading, setLoading] = useState(false)
 
   function handleGoogleSignIn() {
-    signIn('google', { callbackUrl: 'http://localhost:3000/app/today' }, { prompt: "login" })
+    signIn('google', { callbackUrl: '/app/today' }, { prompt: "login" })
   }
 
   function handleFacebookSignIn() {
-    signIn('facebook', { callbackUrl: 'http://localhost:3000/app/today' }, { prompt: "login" })
+    signIn('facebook', { callbackUrl: '/app/today' }, { prompt: "login" })
   }
 
 
